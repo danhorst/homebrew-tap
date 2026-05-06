a63e46f0830810314156fd20aa73e68fa97ece0b
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2026-05-05 22:49:26 -0400

    Update wrk to v1.3.1

--- Formula/wrk.rb
+++ Formula/wrk.rb
@@ -4,8 +4,8 @@
 class Wrk < Formula
   desc "Jump to a git project and create or attach a tmux session"
   homepage "https://github.com/danhorst/wrk"
-  url "https://github.com/danhorst/wrk/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "3fb25b593fc64061dc0af14c2a31a781100c8abf73586cf52303e2b128e4cc97"
+  url "https://github.com/danhorst/wrk/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "94fa2ac322c575f65b9cb5c071241429a8d157fae9f31aea4e9a890165067b92"
   license "MIT"
 
   depends_on "fzf"
